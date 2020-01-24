4e9a1208757a4b06390cfa8d47f56d97a1771b09
Wellington Mazoni <wmazoni@protonmail.com>
2020-01-24 11:16:36 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,32 @@
+package boardgame;
+
+public class Position {
+	private int row;
+	private int columm;
+	
+	public Position(int row, int columm) {
+		this.row = row;
+		this.columm = columm;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumm() {
+		return columm;
+	}
+
+	public void setColumm(int columm) {
+		this.columm = columm;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + columm;
+	}
+}
